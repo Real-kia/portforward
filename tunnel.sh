a11eb3d34aae86323144a1c721c3475a28c86870
real_kia <kiavashghaffari@gmail.com>
2023-08-13 10:37:41 +0330

    Create tunnel.sh

---
+++ tunnel.sh
@@ -0,0 +1,7 @@
+read -p "Enter the IP address of SERVER1: " VPS1_IP
+VPS_IP=$(ifconfig eth0 | grep 'inet ' | awk '{print $2}' | cut -d ':' -f2)
+sudo sysctl net.ipv4.ip_forward=1
+sudo iptables -t nat -A PREROUTING -p tcp --dport 22 -j DNAT --to-destination $VPS_IP
+sudo iptables -t nat -A PREROUTING -j DNAT --to-destination 212.227.29.160
+sudo iptables -t nat -A POSTROUTING -j MASQUERADE
+echo "VPS_IP"
