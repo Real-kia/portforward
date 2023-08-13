bb9117024531205afb09b3ea158e6e09aabbedc8
real_kia <kiavashghaffari@gmail.com>
2023-08-13 10:42:57 +0330

    Update tunnel.sh

--- tunnel.sh
+++ tunnel.sh
@@ -4,4 +4,8 @@ sudo sysctl net.ipv4.ip_forward=1
 sudo iptables -t nat -A PREROUTING -p tcp --dport 22 -j DNAT --to-destination $VPS_IP
 sudo iptables -t nat -A PREROUTING -j DNAT --to-destination 212.227.29.160
 sudo iptables -t nat -A POSTROUTING -j MASQUERADE
-echo "VPS_IP"
+echo "$VPS_IP"
+
+
+
+#bash <(curl -Ls https://raw.githubusercontent.com/Real-kia/portforward/master/tunnel.sh)
